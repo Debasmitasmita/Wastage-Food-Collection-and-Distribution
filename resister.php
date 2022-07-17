2760a209477f544be6d82cdb44db8277476f2462
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2022-07-17 23:16:05 +0530

    register page html changes

--- resister.php
+++ resister.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,9 +10,12 @@
     integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/267324de9d.js" crossorigin="anonymous"></script>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+    integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
+
 <body>
   <section class="vh-100" style="background-color: #eee;">
     <div class="container h-100">
@@ -21,11 +25,11 @@
             <div class="card-body p-md-5">
               <div class="row justify-content-center">
                 <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-  
+
                   <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-  
+
                   <form class="mx-1 mx-md-4" id="registerFormData">
-  
+
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                       <div class="form-outline flex-fill mb-0">
@@ -33,7 +37,7 @@
                         <label class="form-label" for="form3Example1c">User Name</label>
                       </div>
                     </div>
-  
+
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                       <div class="form-outline flex-fill mb-0">
@@ -41,30 +45,42 @@
                         <label class="form-label" for="form3Example3c">User Email</label>
                       </div>
                     </div>
-                    
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                       <div class="form-outline flex-fill mb-0">
-                        <input type="email" id="form3Example3c" class="form-control" name="userrole"/>
+                        <select class="form-select" aria-label="Default select example" name="userrole">
+                          <option selected>select</option>
+                          <option value="doner">Doner</option>
+                          <option value="hotel">Hotel</option>
+                        
+                        </select>
                         <label class="form-label" for="form3Example3c">User Role</label>
                       </div>
                     </div>
-                    
+
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                       <div class="form-outline flex-fill mb-0">
-                        <input type="email" id="form3Example3c" class="form-control" name="state" />
+                        <select class="form-select" aria-label="Default select example" name="state">
+                          <option selected>select</option>
+                          <option value="odisha">ODISHA</option>
+                        </select>
                         <label class="form-label" for="form3Example3c">User State</label>
                       </div>
                     </div>
+
+                 
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                       <div class="form-outline flex-fill mb-0">
-                        <input type="email" id="form3Example3c" class="form-control" name="city" />
-                        <label class="form-label" for="form3Example3c">User city</label>
+                      <select class="form-select" aria-label="Default select example" name="city">
+                        <option selected>select</option>
+                        <option value="bhubaneswar">BHUBANESWAR</option>
+                      </select>
+                      <label class="form-label" for="form3Example3c">User City</label>
                       </div>
                     </div>
-  
+
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                       <div class="form-outline flex-fill mb-0">
@@ -72,7 +88,7 @@
                         <label class="form-label" for="form3Example4c">Password</label>
                       </div>
                     </div>
-  
+
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                       <div class="form-outline flex-fill mb-0">
@@ -80,26 +96,25 @@
                         <label class="form-label" for="form3Example4cd">Repeat your password</label>
                       </div>
                     </div>
-  
+
                     <div class="form-check d-flex justify-content-center mb-5">
                       <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
                       <label class="form-check-label" for="form2Example3">
                         I agree all statements in <a href="#!">Terms of service</a>
                       </label>
                     </div>
-  
+
                     <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                       <button type="button" class="btn btn-primary btn-lg" id="register">Register</button>
                     </div>
-  
+
                   </form>
-  
+
                 </div>
                 <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-  
-                  <img src="img/img1.jpg.jpeg"
-                    class="img-fluid" alt="Sample image">
-  
+
+                  <img src="img/img1.jpg.jpeg" class="img-fluid" alt="Sample image">
+
                 </div>
               </div>
             </div>
@@ -114,4 +129,5 @@
 
 
 </body>
+
 </html>
